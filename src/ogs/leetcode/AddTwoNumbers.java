be02ceeb34a6ddef93774e5bbc1bca723e5af2d0
Georgy <gogaopel@gmail.com>
2022-01-02 17:48:46 +0300

    finish AddTwoNumbers (need refacroting)

--- src/ogs/leetcode/AddTwoNumbers.java
+++ src/ogs/leetcode/AddTwoNumbers.java
@@ -34,20 +34,31 @@ public class AddTwoNumbers {
         ListNode m3 = new ListNode(9);
         ListNode m2 = new ListNode(9, m3);
         ListNode l2 = new ListNode(9, m2);
-/*
-[9,9,9,9,9,9,9]
-[9,9,9,9]
+        /*
+        [9,9,9,9,9,9,9]
+        [9,9,9,9]
 
- */
+         */
         int buff = 0;
         ListNode buffer = null;
         ListNode l = null;
         do {
-            //add npe check and sum for list with different length
-            int res = l1.val + l2.val + buff;
-            if (res > 10) {
+            int val1 = 0;
+            int val2 = 0;
+            ListNode next1 = null;
+            ListNode next2 = null;
+            if (l1 != null) {
+                val1 = l1.val;
+                next1 = l1.next;
+            }
+            if (l2 != null) {
+                val2 = l2.val;
+                next2 = l2.next;
+            }
+            int res = val1 + val2 + buff;
+            if (res >= 10) {
                 buff = 1;
-                res = res%10;
+                res = res % 10;
             } else {
                 buff = 0;
             }
@@ -55,10 +66,10 @@ public class AddTwoNumbers {
             l = new ListNode(res, buffer);
             buffer = l;
 
-            l1 = l1.next;
-            l2 = l2.next;
-        } while (l1 != null || l2 != null);
-
+            l1 = next1;
+            l2 = next2;
+        } while (buff > 0 || l1 != null || l2 != null);
+        
         return reverse(buffer);
     }
 
